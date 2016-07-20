9376daa49f7e008d25102997c573f8cb33a22518
zhangjia517@hotmail.com <zhangjia517@hotmail.com>
2016-07-20 11:33:44 +0800

    Add Role&Input Controller

---
+++ Client/Assets/Script/InputController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputController : MonoBehaviour
+{
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Client/Assets/Script/RoleController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RoleController : MonoBehaviour
+{
+
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+
+    }
+}
