2f9c3d4ec532a040d2c6cd99a4452d55a10092d7
Foskill <zhangjia517@hotmail.com>
2016-07-26 23:05:30 +0800

    LM

--- Client/Assets/Script/TennisBallLogic.cs
+++ Client/Assets/Script/TennisBallLogic.cs
@@ -6,6 +6,12 @@ public class TennisBallLogic : MonoBehaviour
     private Vector3 m_endPos = new Vector3();
 
     private Vector3 m_p1 = new Vector3();
+    private Vector3 m_p2 = new Vector3();
+    private Vector3 m_p3 = new Vector3();
+    private Vector3 m_p4 = new Vector3();
+    private Vector3 m_p5 = new Vector3();
+
+    private RoleController m_RoleController;
 
     public Vector3 StartPos
     {
@@ -19,6 +25,7 @@ public class TennisBallLogic : MonoBehaviour
             m_startPos = value;
         }
     }
+
     public Vector3 EndPos
     {
         get
@@ -34,11 +41,7 @@ public class TennisBallLogic : MonoBehaviour
 
     private void Start()
     {
-        m_startPos = new Vector3(RoleController.thisScript.transform.position.x, 0.2f, RoleController.thisScript.transform.position.z);
-        m_endPos = new Vector3(RoleController.thisScript.transform.position.x + 1, 0.05f, 1.6f);
-
-        m_p1 = Vector3.Lerp(StartPos, EndPos, 0.35f);
-        Debug.Log(m_p1);
+        m_RoleController = RoleController.thisScript;
     }
 
     private void Update()
@@ -47,26 +50,30 @@ public class TennisBallLogic : MonoBehaviour
 
     public void SetRolePos()
     {
-        gameObject.transform.position = new Vector3(RoleController.thisScript.transform.position.x,
-                                                    0.2f,
-                                                    RoleController.thisScript.transform.position.z);
+        m_startPos = new Vector3(m_RoleController.transform.position.x, 0.2f, m_RoleController.transform.position.z);
+        m_endPos = new Vector3(m_RoleController.transform.position.x + 0.5f, 0.05f, 1.8f);
+
+        m_p1 = Vector3.Lerp(StartPos, EndPos, 0.2f);
+        m_p2 = Vector3.Lerp(StartPos, EndPos, 0.4f);
+        m_p3 = Vector3.Lerp(StartPos, EndPos, 0.6f);
+        m_p4 = Vector3.Lerp(StartPos, EndPos, 0.8f);
+        m_p5 = Vector3.Lerp(StartPos, EndPos, 0.85f);
+
+        gameObject.transform.position = m_startPos;
     }
 
     public void Move()
     {
         iTween.Stop(gameObject);
 
-        gameObject.MoveTo(new Vector3[] { 
-                                          //new Vector3(RoleController.thisScript.transform.position.x, 0.05f, 0.666f),
-
-                                          //new Vector3(RoleController.thisScript.transform.position.x, 0.15f, 1.3f),
-                                          //new Vector3(RoleController.thisScript.transform.position.x, 0.05f, 1.5f),
-
-                                          //new Vector3(RoleController.thisScript.transform.position.x, 0.08f, 1.55f),
-
-                                          new Vector3(m_p1.x, 0.3f, m_p1.z),
-                                          m_endPos,
-                          }, 2f,
+        gameObject.MoveTo(new Vector3[] {new Vector3(m_p1.x, 0.45f, m_p1.z),
+                                         new Vector3(m_p2.x, 0.05f, m_p2.z),
+                                         new Vector3(m_p3.x, 0.25f, m_p3.z),
+                                         new Vector3(m_p4.x, 0.05f, m_p4.z),
+                                         new Vector3(m_p5.x, 0.2f, m_p5.z),
+                                         m_endPos,
+                          },
+                          2f,
                           0f,
                           EaseType.linear);
     }
