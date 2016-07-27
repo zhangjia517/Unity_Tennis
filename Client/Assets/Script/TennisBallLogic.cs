d4a4396aba428617fe1025cce9a7b8965d3c1350
zhangjia517@hotmail.com <zhangjia517@hotmail.com>
2016-07-27 15:09:47 +0800

    LM

--- Client/Assets/Script/TennisBallLogic.cs
+++ Client/Assets/Script/TennisBallLogic.cs
@@ -12,6 +12,7 @@ public class TennisBallLogic : MonoBehaviour
     private Vector3 m_p5 = new Vector3();
 
     private RoleController m_RoleController;
+    private TrailRenderer m_trail;
 
     public Vector3 StartPos
     {
@@ -42,6 +43,7 @@ public class TennisBallLogic : MonoBehaviour
     private void Start()
     {
         m_RoleController = RoleController.thisScript;
+        m_trail = GetComponent<TrailRenderer>();
     }
 
     private void Update()
@@ -50,27 +52,27 @@ public class TennisBallLogic : MonoBehaviour
 
     public void SetRolePos()
     {
+        m_trail.enabled = false;
         m_startPos = new Vector3(m_RoleController.transform.position.x, 0.2f, m_RoleController.transform.position.z);
-        m_endPos = new Vector3(m_RoleController.transform.position.x - 1f, 0.05f, 1.5f);
-
-        m_p1 = ErYuanYiCiForZeroPoint(m_startPos, m_endPos);
-        m_p2 = Vector3.Lerp(StartPos, EndPos, 0.7f);
-        m_p3 = Vector3.Lerp(StartPos, EndPos, 0.85f);
-        m_p4 = Vector3.Lerp(StartPos, EndPos, 0.9f);
-        m_p5 = Vector3.Lerp(StartPos, EndPos, 0.95f);
-
+        m_endPos = new Vector3(m_RoleController.transform.position.x - 1f, 0.05f, 1.8f);
         gameObject.transform.position = m_startPos;
+
+        m_p1 = ErYuanYiCiForZeroPoint(m_startPos, m_endPos, 0);
+        m_p2 = ErYuanYiCiForZeroPoint(m_startPos, m_endPos, 1);
+        m_p3 = ErYuanYiCiForZeroPoint(m_startPos, m_endPos, 1.5f);
+        m_p4 = ErYuanYiCiForZeroPoint(m_startPos, m_endPos, 1.7f);
+        m_p5 = ErYuanYiCiForZeroPoint(m_startPos, m_endPos, 1.75f);
     }
 
     public void Move()
     {
+        m_trail.enabled = true;
         iTween.Stop(gameObject);
-
         gameObject.MoveTo(new Vector3[] {new Vector3(m_p1.x, 0.45f, m_p1.z),
                                          new Vector3(m_p2.x, 0.05f, m_p2.z),
-                                         new Vector3(m_p3.x, 0.2f, m_p3.z),
+                                         new Vector3(m_p3.x, 0.15f, m_p3.z),
                                          new Vector3(m_p4.x, 0.05f, m_p4.z),
-                                         new Vector3(m_p5.x, 0.1f, m_p5.z),
+                                         new Vector3(m_p5.x, 0.07f, m_p5.z),
                                          m_endPos,
                           },
                           2f,
@@ -78,13 +80,13 @@ public class TennisBallLogic : MonoBehaviour
                           EaseType.linear);
     }
 
-    public Vector3 ErYuanYiCiForZeroPoint(Vector3 s, Vector3 e)
+    public Vector3 ErYuanYiCiForZeroPoint(Vector3 s, Vector3 e, float zPos)
     {
         float k, b;
         k = (e.x - s.x) / (e.z - s.z);
         b = s.x - (k * s.z);
 
-        Vector3 centPos = new Vector3(b, 0.5f, 0);
+        Vector3 centPos = new Vector3(zPos * k + b, 0.5f, zPos);
         return centPos;
     }
 }
